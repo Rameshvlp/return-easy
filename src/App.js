43e55d4441e8716a10d5b8b0d17565bafa17637b
Rameshvlp <rameshvlp2001@gmail.com>
2023-11-13 16:42:59 +0530

    trello

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from "./components/Home/home";
+import Navbar from "./components/Navbar";
+import "./index.scss";
+
+const App = () => {
+  return (
+  <div>
+    
+   <Navbar /> 
+   <Home />
+  </div>
+  );
+
+};
+
+export default App;
+
+
